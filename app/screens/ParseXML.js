ac41aef5f5b9c80ea64a23b8ed9a110753c4341a
cikai <cikai0923@gmail.com>
2017-04-26 14:17:08 +0800

    xml parser

--- app/screens/ParseXML.js
+++ app/screens/ParseXML.js
@@ -7,22 +7,35 @@ import {
   View
 } from 'react-native';
 
-import parser from 'xml2json';
-// var parser = require('xml2json');
+import XMLParser from 'react-xml-parser';
 
 const fetchApi = () => {
-  let url = `http://wthrcdn.etouch.cn/WeatherApi?citykey=101010100`;
-  let resp = fetch(url).then((res)=>res);
-  let xml = "<foo attr=\"value\">bar</foo>";
-  let result = parser.toJson(xml);
-  console.log(result);
+  let url = `http://192.168.1.254/?custom=1&cmd=2001&par=1`;
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/xml"
+    }
+  }).then(function(response) {
+    if(response.status === 200){
+      return response.text();
+    }else{
+      return {}
+    }
+  }).then(function(data){
+    console.log(data);
+    var xml = new XMLParser().parseFromString(xmltext.replace(/[\r\n]/g,""));
+    console.log(xml);
+  }).catch(function(err){
+    console.log(err);
+  })
 };
 
 class ParseXML extends Component {
   render() {
     return (
-      <View>
-        <Button title={'fetch'} onPress={fetchApi}></Button>
+      <View style={{flex: 1, margin: 150}}>
+        <Button title={'录制'} onPress={fetchApi}></Button>
       </View>
     );
   }
